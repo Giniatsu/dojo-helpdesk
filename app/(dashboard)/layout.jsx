051c21086d5ba32aff12ae7ea3fd3cd6971ed7a0
Giniatsu <aragarcia@addu.edu.ph>
2023-09-16 14:22:22 +0800

    NextJS13 Course from NetNinja

---
+++ app/(dashboard)/layout.jsx
@@ -0,0 +1,22 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+// components
+import Navbar from "@/app/components/Navbar";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+
+  if(!data.session){
+    redirect('/login')
+  }
+
+  return (
+    <>
+      <Navbar user={data.session.user} />
+      {children}
+    </>
+  );
+}
